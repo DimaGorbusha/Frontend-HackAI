b407f17b8b1c5a7934db5b6d17c40dfd267a8fc3
Dmitriy Gorbunov <dmitrgor1@yandex.ru>
2022-10-28 16:47:45 +0500

    Add: added colors and Footer

---
+++ Frontend/hack-test-front/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        <p className='font-sussie-book text-darkgray ml-[657px]'>gesti.hub 2022</p>
+    </div>
+  )
+}
--- Frontend/hack-test-front/tailwind.config.js
+++ Frontend/hack-test-front/tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        darkgray: "#696969",
+        bg: "#696969"
+
+      },
       fontFamily: {
         'sussie-regular': ['Sussie Intl Regular'],
         'sussie-medium': ['Sussie Intl Medium'],
