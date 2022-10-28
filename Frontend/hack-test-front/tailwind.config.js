1b6b0bcf0a19d1a21851fda4cc25eaf5af6d077d
Dmitriy Gorbunov <dmitrgor1@yandex.ru>
2022-10-28 16:37:54 +0500

    Add: added fonts

--- Frontend/hack-test-front/tailwind.config.js
+++ Frontend/hack-test-front/tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sussie-regular': ['Sussie Intl Regular'],
+        'sussie-medium': ['Sussie Intl Medium'],
+        'sussie-book': ['Sussie Intl Book'],
+      }
+    },
   },
   plugins: [],
 }
