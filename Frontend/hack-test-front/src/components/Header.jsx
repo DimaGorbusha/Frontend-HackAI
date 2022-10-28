e75885b508f5a81571dd69ff1f09408725f15574
Dmitriy Gorbunov <dmitrgor1@yandex.ru>
2022-10-28 16:18:06 +0500

    Add: created header component

---
+++ Frontend/hack-test-front/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from '../img/common/logo.svg'
+
+export default function Header() {
+  return (
+    <div className='flex items-center align-middle text-center'>
+        <img src="" alt="" />
+    </div>
+  )
+}
